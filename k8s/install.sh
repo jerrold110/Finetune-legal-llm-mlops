b98504d63cc518ee0fdcca77cb1d5fe4c8ea3cdd
Jerrold Koh <jerroldfisher@live.com>
2026-09-16 18:42:23 +0800

    Removed mlrun installation timeout, added port forwarding

--- k8s/install.sh
+++ k8s/install.sh
@@ -30,7 +30,7 @@ helm --namespace mlrun \
     install mlrun-ce \
     --version 0.11.0 \
     --wait \
-    --timeout 3600s \
+    #--timeout 3600s \
     --set global.registry.url=$ECR_SERVER \
     --set global.registry.secretName=ecr-build-secret \
     --set global.externalHostAddress=localhost \
