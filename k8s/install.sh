f4d769bef17e845b320ae2776528e9d9792e89e7
Jerrold Koh <jerroldfisher@live.com>
2026-09-19 14:35:32 +0800

    Changed debug to not use -it and extended time limit to 1000

--- k8s/install.sh
+++ k8s/install.sh
@@ -29,7 +29,7 @@ helm --namespace mlrun \
     install mlrun-ce \
     --version 0.11.0 \
     --wait \
-    --timeout 600s \
+    --timeout 1000s \
     --set global.registry.url=$ECR_SERVER \
     --set global.registry.secretName=ecr-build-secret \
     --set global.externalHostAddress=localhost \
