0f8a6a02026041e3205af254aeebd41a4ee31bef
Jerrold Koh <jerroldfisher@live.com>
2026-09-19 15:03:12 +0800

    Removed extra flags to see if causing limit at 600

--- k8s/install.sh
+++ k8s/install.sh
@@ -36,9 +36,6 @@ helm --namespace mlrun \
     --set pipelines.enabled=false \
     --set kube-prometheus-stack.enabled=false \
     --set spark-operator.enabled=false \
-    --set mlrun.db.securityContext.runAsUser=null \
-    --set mlrun.db.podSecurityContext.runAsUser=null \
-    --set mlrun.db.podSecurityContext.fsGroup=null \
     mlrun-ce/mlrun-ce
 
 # Credentials for pods to pull images
